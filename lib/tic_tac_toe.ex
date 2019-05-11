94f7470dec4046516e98e9e8baf113307c2be7c8
rliebling <rich.liebling@sigfig.com>
2019-05-11 16:47:56 -0700

    add more support for operating on a game
    move()
    at_coords()

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -7,6 +7,8 @@ defmodule TicTacToe do
   if it comes from the database, an external API or others.
   """
 
+  require Integer #for is_even
+
   defmodule Game do
     @doc """
       iex> TicTacToe.Game.new
@@ -49,7 +51,7 @@ defmodule TicTacToe do
         {:o, :o, :o} == Game.col(game, 2) -> {true, "O wins"}
         {:o, :o, :o} == Game.diag(game, 0) -> {true, "O wins"}
         {:o, :o, :o} == Game.diag(game, 1) -> {true, "O wins"}
-        Game.moves(game) == 9 -> {true, "Draw!"}
+        game[:moves] == 9 -> {true, "Draw!"}
         true -> {false}
       end
     end
@@ -73,6 +75,46 @@ defmodule TicTacToe do
     end
     def diag(g, 1) do
       Enum.map([0,1,2], fn r -> (Game.row(g, r) |> elem(2-r)) end)
+      |> List.to_tuple
+    end
+
+    def at_coords(g, coords, x_value \\ :x, o_value \\ :o, nil_value \\ nil) do
+      x_or_o = g[:board]
+               |> elem( elem(coords,0) )
+               |> elem( elem(coords,1) )
+      case x_or_o do
+        :x -> x_value
+        :o -> o_value
+        _ -> nil_value
+      end
+    end
+
+    def move(g, coords) do
+      x_or_o = case Integer.is_even(g[:moves]) do
+        true -> :x
+        _ -> :o
+      end
+      case at_coords(g, coords) do
+        nil -> game = %{g | moves: g[:moves]+1, board: replace(g, coords, x_or_o)}
+          case game_over?(game) do
+            {true, msg} -> {:ok, game, msg}
+            _ -> {:ok, game, ""}
+          end
+          _ -> {:error, "#{x_or_o} cannot move to space currently occupied by #{at_coords(g, coords)}"}
+      end
+    end
+
+    defp replace(g, coords, x_or_o) do
+      r = elem(coords, 0)
+      c = elem(coords, 1)
+      g[:board]
+      |> Tuple.to_list
+      |> Enum.with_index |> Enum.map(fn {row, i} -> 
+        case i==r do
+          true -> put_elem(row, c, x_or_o)
+          _ -> row
+        end
+      end)
       |> List.to_tuple
     end
   end
